b3a186694da1b9fa2f756b2926be257b471d3176
DamianoCode <dam1138@gmail.com>
2021-10-19 19:51:30 +0200

    add styles and change html template

--- header.php
+++ header.php
@@ -4,18 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tytuł</title>
     <link rel="stylesheet" href="./styles/style.css">
 </head>
 <body>
-    <header class="header">
-        <div class="header__content content__container">
-            <a class="header__logo" href="">LOGO</a>
+    <header class="navbar">
+            <a class="navbar__logo" href="/index.php">LOGO</a>
 
-            <ul class="header__links">
-                <li class="header__link"><a href="">Lorem ipsum dolor sit.</a></li>
-                <li class="header__link"><a href="">Lorem ipsum dolor sit.</a></li>
-                <li class="header__link"><a href="">Lorem ipsum dolor sit amet.</a></li>
+            <ul class="navbar__links">
+                <li class="navbar__link"><a href="">Lorem ipsum dolor sit.</a></li>
+                <li class="navbar__link"><a href="">Lorem ipsum dolor sit.</a></li>
+                <li class="navbar__link"><a href="">Lorem ipsum dolor sit amet.</a></li>
             </ul>
-        </div>
     </header>
--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php include_once("header.php") ?>
 
 <div class="main__container" id="page-content">
-    <div class="hero__section">
-        <h1>Tytuł strony</h1>
-    </div>
+    <section class="hero__container section">
+		<h1 class="hero__title">Tytuł</h1>
+		<p class="hero__text">krótki opis</p>
+		<p class="text-background">Projekt CMS</p>
+	</section>
     <div class="content__container">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum iusto perferendis facilis soluta reprehenderit porro, in ipsam pariatur adipisci animi accusantium repellat ipsa voluptatem praesentium a quos eius itaque et.
-    Expedita facere pariatur praesentium, labore repellat at culpa saepe, fugit nesciunt recusandae dicta aliquid enim amet dolorum autem doloribus provident excepturi harum rem. Cumque officia sequi nostrum voluptates recusandae reprehenderit.
-    Aliquam architecto, quaerat neque vel ab illo ipsum, consectetur doloribus ut repellat commodi eum! Quidem quis voluptate excepturi! Debitis reiciendis officia corrupti unde explicabo. Quae iusto molestiae non quisquam animi?
-    Repudiandae eius consectetur doloremque, accusamus officia saepe nostrum amet laudantium praesentium possimus minima quos consequuntur omnis iusto sunt delectus beatae, deleniti, optio facere! Impedit, ratione. Magni aliquid quidem sint consequuntur.
-    Saepe veniam quas suscipit ea nobis provident alias corporis harum amet repudiandae aliquid officiis et eum, unde debitis sequi nulla consequatur libero corrupti. Ab exercitationem veritatis dolorum rem, illo distinctio.
-    Voluptate cum sint, sapiente quas amet a molestias autem quibusdam eius quis officia inventore totam id aut deserunt cupiditate aspernatur quod assumenda nihil consequuntur beatae vitae architecto. Mollitia, optio molestias!
-    Quae at et rem hic totam ratione accusantium ipsa rerum incidunt, nulla labore in ullam architecto recusandae eos soluta ab amet modi quibusdam minima nihil explicabo accusamus aliquam! Dolorem, laborum.
-    Totam autem eligendi voluptates modi iste culpa, impedit et voluptatibus harum amet aperiam praesentium, magnam sed tempora iusto cumque nisi, dignissimos laudantium dolorum vel quisquam facilis? Doloremque numquam deleniti perspiciatis.
-    Voluptatum qui in delectus exercitationem tempora aperiam suscipit accusamus facere. Non harum totam deserunt quam adipisci eligendi, asperiores aut, alias, aspernatur excepturi sunt architecto iste error voluptatem dolore dolorum aperiam?
-    Aperiam exercitationem, assumenda voluptate maxime facilis maiores nam unde vel esse nobis doloremque numquam minima blanditiis, fugiat eius perspiciatis, quos officia atque sunt totam aut. Sunt asperiores sit illum a!
-    Quos, quod. Excepturi debitis hic deleniti quod minus ipsa quis voluptatibus necessitatibus impedit laboriosam quam eos saepe animi, in accusantium error. Eaque provident quibusdam deserunt ea tenetur suscipit delectus soluta?
-    Voluptatem, velit. Blanditiis quo veniam est sint id doloremque nulla non magnam minima, iste nesciunt earum eos consequuntur quasi error sunt animi corrupti molestiae facilis. Natus tenetur illum suscipit quas.
-    Labore omnis deserunt consequuntur ipsum. Suscipit sunt officia iste impedit perspiciatis nobis amet inventore numquam mollitia, eligendi vitae, distinctio, est dicta commodi cupiditate odio. Doloribus qui placeat totam necessitatibus sint.
-    Distinctio voluptas provident dolor obcaecati inventore at aspernatur quam voluptate ut quo. Accusantium nemo quaerat voluptatibus molestiae? Corporis perspiciatis blanditiis corrupti dolor, sint expedita a deleniti atque omnis rerum pariatur!
-    Vitae eum velit quaerat adipisci, laudantium incidunt iusto officiis saepe consectetur obcaecati provident. Molestias harum ullam nesciunt quia illo, molestiae dolorem laborum error vero recusandae voluptas facilis quae non placeat.
-    Voluptas dolor soluta fuga in nostrum modi magni fugiat necessitatibus quisquam? uaerat sit! Sapiente architecto minima pariatur cumque nemo qui adipisci.
-    </div>
+        <div class="text__container">
+            <h2>Podtytuł 1</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium alias maxime adipisci! Veritatis similique vero deleniti dicta non magni ad nesciunt, iusto, consectetur facilis unde? Provident quod fugit cupiditate consequuntur.
+            Veniam dolorum repudiandae minima, in ab ipsum cum, et explicabo dignissimos vitae eius mollitia quidem ad provident ratione, possimus exercitationem aliquid? Eligendi placeat quis maxime. Nostrum error dignissimos labore veritatis.
+            Fugiat saepe beatae, ab fuga iusto molestiae quisquam blanditiis tempore nisi animi sint provident assumenda suscipit illo reiciendis numquam quos dolorum corporis quis? Repellendus illum totam officiis in, blanditiis similique.
+            Dolores aperiam dolore ducimus dicta in labore rem minima quibusdam a magnam nostrum explicabo illum ullam earum ipsam sint soluta fugit assumenda, expedita molestias? Perspiciatis commodi cumque unde alias minus?
+            Eos dolorum amet iste obcaecati suscipit voluptatum delectus! Dolorum quae nesciunt sit incidunt ullam autem, ad doloribus error iste iure qui distinctio officiis nemo ea totam. Aliquid fugiat dignissimos voluptates.
+            Molestiae suscipit eveniet necessitatibus, omnis fugit, adipisci magnam sequi animi praesentium minus sapiente natus eligendi autem cumque nostrum quisquam cum reprehenderit quam. Rem aut porro eveniet natus voluptate maiores officia!
+        </div>
+        <div class="text__container">
+            <h2>Podtytuł 2</h2>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et modi nisi saepe sapiente iusto tempora porro! Itaque iusto, quasi ipsum fugiat laudantium beatae nihil maxime, deleniti earum commodi, esse incidunt.
+            Natus, voluptas fugiat ab facilis saepe non necessitatibus tempora et voluptatibus doloremque dolor nihil officia ad possimus rem ex commodi voluptates aliquid minus. Similique expedita vel, animi inventore natus nihil!
+            Quam, rem nesciunt eos amet numquam dolorum repellat adipisci. Nisi voluptatum officiis quisquam incidunt eum laudantium hic velit vero sit! Dolorem totam, consectetur molestiae corrupti laboriosam labore temporibus quaerat libero.
+        </div>
 </div>
 
 <?php include_once("footer.php") ?>
